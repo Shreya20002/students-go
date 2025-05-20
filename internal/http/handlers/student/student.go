f3a0d1ec520f3c2eb2509bb622ca5b8b84dc8a41
Shreya <20BCS209@iiitdmj.ac.in>
2025-05-20 17:36:37 +0530

    add get student by id

--- internal/http/handlers/student/student.go
+++ internal/http/handlers/student/student.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"strconv"
 
 	"github.com/Shreya20002/students-go/internal/storage"
 	"github.com/Shreya20002/students-go/internal/types"
@@ -58,4 +59,27 @@ func New(storage storage.Storage) http.HandlerFunc {
 	//response.WriteJson(w, http.StatusCreated, map[string]string{"success": "ok"})
 	//w.Write([]byte("welcome to students api"))
 
+}
+
+func GetById(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		slog.Info("getting a student", slog.String("id", id))
+
+		intId, err := strconv.ParseInt(id, 10, 64)
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+
+		student, err := storage.GetStudentById(intId)
+
+		if err != nil {
+			slog.Error("error getting user", slog.String("id", id))
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+
+		response.WriteJson(w, http.StatusOK, student)
+	}
 }
