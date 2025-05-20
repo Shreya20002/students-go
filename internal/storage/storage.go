ee7b8f0a4e983268516106eef78296e4026f45a4
Shreya <20BCS209@iiitdmj.ac.in>
2025-05-20 17:57:29 +0530

    add get student list

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,5 +5,5 @@ import "github.com/Shreya20002/students-go/internal/types"
 type Storage interface {
 	CreateStudent(name string, email string, age int) (int64, error)
 	GetStudentById(id int64) (types.Student, error)
-	//GetStudents() ([]types.Student, error)
+	GetStudents() ([]types.Student, error)
 }
